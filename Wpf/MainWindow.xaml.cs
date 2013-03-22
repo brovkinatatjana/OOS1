6e8eee020f7a5e3ef80eff8166e43786ba7c489d
brovkinatatjana <brovkinatatjana@rambler.ru>
2013-03-22 14:38:15 +0400

    C3

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -125,7 +125,7 @@ namespace Wpf
             lbMain.Items.Clear();
             for (index = 1; index <= itemCount; index++)
             {
-                number = -100 + rnd1.Next(200);
+                number = -100 + rnd1.Next(20);
                 myAL.Add(number);
                 lbMain.Items.Add(number);
             }
@@ -145,6 +145,31 @@ namespace Wpf
                 lbMain.Items.Add("Не существует");
         }
 
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
 
+            int sum = 0;
+            int[] ar1 = new Int32[itemCount];
+            myAL.CopyTo(ar1);
+            for (index = 0; index < itemCount - 1; index++)
+            {
+                if ((ar1[index] > ar1[1]))
+                    sum += ar1[index];
+            }
+            lbMain.Items.Add("Сумма элементов больших, чем   второй элемент этого массива");
+            lbMain.Items.Add(sum);
+        }
     }
 }
