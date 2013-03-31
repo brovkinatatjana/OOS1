7a939bac80d122823754b252e71b7924661c6996
brovkinatatjana <brovkinatatjana@rambler.ru>
2013-03-31 14:35:19 +0400

    C7

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -199,5 +199,33 @@ namespace Wpf
             lbMain.Items.Add("элементов больших, чем   4 элемент этого массива");
             lbMain.Items.Add(tum);
         }
+
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+
+            int sum = 0;
+            int[] ar1 = new Int32[itemCount];
+            myAL.CopyTo(ar1);
+            for (index = 0; index < itemCount - 1; index++)
+            {
+                if ((ar1[index] < 21))
+                    sum += ar1[index];
+            }
+            lbMain.Items.Add("Сумма элементов меньших 21");
+            lbMain.Items.Add(sum);
+        }
     }
 }
