821b24c8d3a07fbe0303c5aec34a0b20d9d9761f
brovkinatatjana <brovkinatatjana@rambler.ru>
2013-06-05 21:18:13 +0400

    С5_2

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -202,5 +202,217 @@ namespace Wpf
             lbMain.Items.Add("Знак меняется");
             lbMain.Items.Add(tum);
         }
+
+        private void button_6_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+
+            int tum = 0;
+            int[] ar1 = new Int32[itemCount];
+            myAL.CopyTo(ar1);
+            for (index = 0; index < itemCount; index++)
+            {
+                if ((ar1[index] > ar1[3]))
+                    tum++;
+            }
+            lbMain.Items.Add("элементов больших, чем   4 элемент этого массива");
+            lbMain.Items.Add(tum);
+        }
+
+
+        private void button_7_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+
+            int sum = 0;
+            int[] ar1 = new Int32[itemCount];
+            myAL.CopyTo(ar1);
+            for (index = 0; index < itemCount - 1; index++)
+            {
+                if ((ar1[index] < 21))
+                    sum += ar1[index];
+            }
+            lbMain.Items.Add("Сумма элементов меньших 21");
+            lbMain.Items.Add(sum);
+        }
+
+       
+
+
+        private void button_8_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+
+
+            lbMain.Items.Add("        ==        ");
+            int sum = 0;
+            int[] ar1 = new Int32[itemCount];
+            myAL.CopyTo(ar1);
+            for (index = 0; index < itemCount; index++)
+            {
+                if (ar1[index] % 5 == 0)
+                    ar1[index] = ar1[index] + 1;
+                lbMain.Items.Add(ar1[index]);
+            }
+        }
+
+
+        private void button_9_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+
+            int sum = 0;
+            int[] ar1 = new Int32[itemCount];
+            myAL.CopyTo(ar1);
+            for (index = 0; index < itemCount; index++)
+            {
+                if (ar1[index] % 3 != 0)
+                    sum += ar1[index];
+            }
+            lbMain.Items.Add("Сумма элементов, не кратных 3");
+            lbMain.Items.Add(sum);
+        }
+
+
+        private void button_10_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                ArrayList myAL = new ArrayList();
+                int index;
+                int itemCount = Convert.ToInt32(tbN.Text);
+                Random rnd1 = new Random();
+                int number;
+                lbMain.Items.Clear();
+                for (index = 1; index <= itemCount; index++)
+                {
+                    number = -100 + rnd1.Next(200);
+                    myAL.Add(number);
+                    lbMain.Items.Add(number);
+                }
+
+                int sum = 0;
+                int[] ar1 = new Int32[itemCount];
+                myAL.CopyTo(ar1);
+                for (index = 1; index < itemCount - 1; index++)
+                {
+                    if ((ar1[index] < ar1[0]) && (ar1[index] > ar1[itemCount - 1]))
+                        sum++;
+                }
+                lbMain.Items.Add("элементов  <1 и >последнего");
+                lbMain.Items.Add(sum);
+            }
+        }
+
+
+        private void button_12_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                ArrayList myAL = new ArrayList();
+                int index;
+                int itemCount = Convert.ToInt32(tbN.Text);
+                Random rnd1 = new Random();
+                int number;
+                lbMain.Items.Clear();
+                for (index = 1; index <= itemCount; index++)
+                {
+                    number = -100 + rnd1.Next(200);
+                    myAL.Add(number);
+                    lbMain.Items.Add(number);
+                }
+
+                int min1 = 0, min2 = 0;
+                int[] ar1 = new Int32[itemCount];
+                myAL.CopyTo(ar1);
+                min1 = Math.Abs(ar1[0] - ar1[1]);
+                for (index = 2; index < itemCount; index++)
+                {
+
+                    if (Math.Abs(ar1[index] - ar1[1]) < min1)
+                    {
+                        min1 = Math.Abs(ar1[index] - ar1[1]);
+                        min2 = index;
+                    }
+                }
+                lbMain.Items.Add("Элемент, меньше всего отличающийся от второго");
+                lbMain.Items.Add(min2 + 1);
+            }
+        }
+
+
+
+        private void button_14_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                ArrayList myAL = new ArrayList();
+                int index;
+                int itemCount = Convert.ToInt32(tbN.Text);
+                Random rnd1 = new Random();
+                int number;
+                lbMain.Items.Clear();
+                for (index = 1; index <= itemCount; index++)
+                {
+                    number = -100 + rnd1.Next(200);
+                    myAL.Add(number);
+                    lbMain.Items.Add(number);
+                }
+                int[] ar1 = new Int32[itemCount];
+                myAL.CopyTo(ar1);
+                int tum = 0;
+                for (index = 0; index < itemCount; index++)
+                {
+
+                    if (ar1[index] % 3 == 0)
+                    {
+                        tum++;
+                    }
+                }
+                lbMain.Items.Add("Элементов, кратных 3, ");
+                lbMain.Items.Add(tum);
+            }
+        }
     }
 }
